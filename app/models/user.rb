d678a731e9d907ddf5cf432f8f3ed3f58bed6006
SriGaneshVidhya <ganesh.s01@outlook.com>
2020-01-30 21:46:25 +0530

    Validations Added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-
+  validates :name, presence: true,
+  uniqueness: {case-sensitive: false},
+  length: {minimum: 3, maximum: 25}
+  EMAIL_FORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: EMAIL_FORMAT },
+  uniqueness: {case-sensitive: false}
+  length: {minimum: 3, maximum: 75}
 end
