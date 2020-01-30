8c796d58c5e534119e0a8ba3dc42a7f1de451dba
SriGaneshVidhya <ganesh.s01@outlook.com>
2020-01-30 21:37:19 +0530

    Users Created

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200128200822) do
+ActiveRecord::Schema.define(version: 20200130160230) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.integer "age"
     t.integer "cost"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
