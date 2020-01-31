493d465c8b78dd4916adc23b99b3d37d6a944350
SriGaneshVidhya <ganesh.s01@outlook.com>
2020-01-31 19:03:34 +0530

    sign_up_window_created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "products#new"
   # get "/list", to: "products#list"
-  resources :products
-
   get '/home', to: "products#home"
+  resources :products
+  # resources :users
+  get '/signup', to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
