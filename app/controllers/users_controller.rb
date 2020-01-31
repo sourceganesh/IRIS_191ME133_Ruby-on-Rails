f1815e8fdf70ccf3c2250e3da8a0adc383bf7a8d
SriGaneshVidhya <ganesh.s01@outlook.com>
2020-01-31 19:44:08 +0530

    sign-up-page-created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
 
   def new
-    @user = User.new 
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      flash[:error] = "Invalid"
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # get "/list", to: "products#list"
   get '/home', to: "products#home"
   resources :products
-  # resources :users
-  get '/signup', to: "users#new"
+  resources :users
+  get 'signup', to: "users#new"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
