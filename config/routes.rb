edcf395190efe1c82adebce2a0e9a0cf369a5db5
SriGaneshVidhya <ganesh.s01@outlook.com>
2020-01-31 21:58:46 +0530

    sessions_added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+
   root "products#new"
-  # get "/list", to: "products#list"
-  get '/home', to: "products#home"
+  get 'home', to: "products#home"
   resources :products
+  # get '/list', to: "products#list"
+
   resources :users
-  get '/signup', to: "users#new"
+  get 'signup', to: "users#new"
+  # okay nvm
+
+  get 'login', to:"sessions#new"
+  post 'login', to:"sessions#create"
+  delete 'login', to:"sessions#delete"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
